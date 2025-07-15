72fea7d68bc6e1f56637b6932fd3b152139a47e3
methrick <134304016+methrick@users.noreply.github.com>
2025-07-15 12:14:27 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,3 +1,4 @@
-resource "aws_iam_user" "user" {
-  name = "test-user"
+resource "aws_iam_user_policy_attachment" "user" {
+  user = "aws_iam_user.cloud.name"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
