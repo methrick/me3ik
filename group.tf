865368dbd44172fabcb51529714d76888fa0f386
methrick <134304016+methrick@users.noreply.github.com>
2025-07-15 06:47:15 +0000

    modified

--- group.tf
+++ group.tf
@@ -2,8 +2,3 @@ resource "aws_iam_group" "group" {
 	name = "test-group"
 }
 
-resource "aws_iam_group_membership" "group" {
-	name = "qwerty"
-	user = "aws_iam_user.cloud.name"
-	group = "aws_iam_group.group.name"
-}
--- user.tf
+++
@@ -1,4 +0,0 @@
-
-resource "aws_iam_user" "cloud1" {
-  name = "loadbalancer"
-}
