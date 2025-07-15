9dc80d95d02a754d6aa67f1ccb111e2c4a9e48e0
methrick <134304016+methrick@users.noreply.github.com>
2025-07-15 11:47:44 +0530

    Update group.tf

--- group.tf
+++ group.tf
@@ -2,3 +2,8 @@ resource "aws_iam_group" "group" {
 	name = "test-group"
 }
 
+resource "aws_iam_group_membership" "group" {
+	name = "qwerty"
+	user = "aws_iam_user.cloud.name"
+	group = "aws_iam_group.group.name"
+}
